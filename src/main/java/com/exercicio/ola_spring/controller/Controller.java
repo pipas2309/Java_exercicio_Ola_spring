d8073964c1df1954fc991173d0db36fa276856e3
Lucas Palharini <lucas230991@hotmail.com>
2023-01-18 21:46:44 -0300

    :bug: fix: remove wrong import

--- src/main/java/com/exercicio/ola_spring/controller/Controller.java
+++ src/main/java/com/exercicio/ola_spring/controller/Controller.java
@@ -1,7 +1,6 @@
 package com.exercicio.ola_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
