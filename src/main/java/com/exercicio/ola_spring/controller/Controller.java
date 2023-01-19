8f0839fad95b8f62e181f8852222c2510a557dbe
Lucas Palharini <lucas230991@hotmail.com>
2023-01-18 21:37:34 -0300

    :sparkles: feat: add query param

--- src/main/java/com/exercicio/ola_spring/controller/Controller.java
+++ src/main/java/com/exercicio/ola_spring/controller/Controller.java
@@ -1,14 +1,24 @@
 package com.exercicio.ola_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
+
+    @ResponseBody
     @GetMapping("/get")
-    public String olaSpring() {
-        return "Olá Spring";
+    public String olaSpring(@RequestParam(required = false) String nome) {
+        if(nome == null || nome.equals("")) {
+            return "Escreva seu nome no link dessa forma ?nome=SEU_NOME";
+        } else {
+            return "Cuidado, " + nome + ", agora você está num esquema de pirâmide e precisa bater a meta!";
+        }
+        
     }
 }
