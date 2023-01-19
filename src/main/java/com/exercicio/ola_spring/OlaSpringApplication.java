86a6a21ab1d67e8c4879354ad1ef080bd1543a0e
Lucas Palharini <lucas230991@hotmail.com>
2023-01-18 21:21:56 -0300

    :tada: Initial commit

---
+++ src/main/java/com/exercicio/ola_spring/OlaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.ola_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercicio/ola_spring/OlaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.ola_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
